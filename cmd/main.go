3b0fe5b7bce10183026845365ff5bdff6cfa28f6
Troy Kessler <troy.kessler99@gmail.com>
2023-03-09 17:36:53 +0100

    continued on blockchain sync reactor

--- cmd/main.go
+++ cmd/main.go
@@ -2,40 +2,19 @@ package main
 
 import (
 	"KYVENetwork/kyve-tm-bsync/blocks"
-	log "KYVENetwork/kyve-tm-bsync/logger"
 	"KYVENetwork/kyve-tm-bsync/sync"
-	cfg "KYVENetwork/kyve-tm-bsync/sync/config"
-	s "KYVENetwork/kyve-tm-bsync/sync/state"
 	"KYVENetwork/kyve-tm-bsync/types"
 	"fmt"
-)
-
-var (
-	logger = log.Logger()
 )
 
 func main() {
 	homeDir := "/Users/troykessler/.chain"
 
-	config, err := cfg.LoadConfig(homeDir)
-	if err != nil {
-		panic(fmt.Errorf("failed to load config: %w", err))
-	}
-
-	logger.Info(config.Moniker)
-
-	state, err := s.GetState(config)
-	if err != nil {
-		panic(fmt.Errorf("failed to load state: %w", err))
-	}
-
-	logger.Info(fmt.Sprintf("%d", state.LastBlockHeight))
-
 	blockCh := make(chan *types.Block)
 	quitCh := make(chan int)
 
 	go blocks.NewBundlesReactor(blockCh, quitCh, 0, 0, 0)
-	go sync.NewBlockSyncReactor(blockCh, quitCh)
+	go sync.NewBlockSyncReactor(blockCh, quitCh, homeDir)
 
 	<-quitCh
 
--- sync/sync.go
+++ sync/sync.go
@@ -1,15 +1,54 @@
 package sync
 
 import (
+	log "KYVENetwork/kyve-tm-bsync/logger"
+	cfg "KYVENetwork/kyve-tm-bsync/sync/config"
+	s "KYVENetwork/kyve-tm-bsync/sync/state"
 	"KYVENetwork/kyve-tm-bsync/types"
 	"fmt"
+	c "github.com/tendermint/tendermint/config"
+	"github.com/tendermint/tendermint/proxy"
 )
 
-func NewBlockSyncReactor(blockCh <-chan *types.Block, quitCh <-chan int) {
+var (
+	logger = log.Logger()
+)
+
+func createAndStartProxyAppConns(config *c.Config) (proxy.AppConns, error) {
+	proxyApp := proxy.NewAppConns(proxy.DefaultClientCreator(config.ProxyApp, config.ABCI, config.DBDir()))
+	proxyApp.SetLogger(logger.With("module", "proxy"))
+	if err := proxyApp.Start(); err != nil {
+		return nil, fmt.Errorf("error starting proxy app connections: %v", err)
+	}
+	return proxyApp, nil
+}
+
+func NewBlockSyncReactor(blockCh <-chan *types.Block, quitCh <-chan int, homeDir string) {
+	config, err := cfg.LoadConfig(homeDir)
+	if err != nil {
+		panic(fmt.Errorf("failed to load config: %w", err))
+	}
+
+	logger.Info(fmt.Sprintf("Config loaded. Moniker = %s", config.Moniker))
+
+	state, err := s.GetState(config)
+	if err != nil {
+		panic(fmt.Errorf("failed to load state: %w", err))
+	}
+
+	logger.Info(fmt.Sprintf("State loaded. LatestBlockHeight = %d", state.LastBlockHeight))
+
+	proxyApp, err := createAndStartProxyAppConns(config)
+	if err != nil {
+		panic(fmt.Errorf("failed to start proxy app: %w", err))
+	}
+
+	_ = proxyApp
+
 	for {
 		select {
 		case block := <-blockCh:
-			fmt.Printf("%v %s\n", block.Header.Height, block.Header.AppHash)
+			logger.Info(fmt.Sprintf("%v %s", block.Header.Height, block.Header.AppHash))
 		case <-quitCh:
 			return
 		}
