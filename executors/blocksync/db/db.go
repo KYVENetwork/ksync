cae30c938c5053dd9c0cdd2fa97c0ccbd5a38134
Troy Kessler <troy.kessler99@gmail.com>
2023-09-18 11:45:34 +0200

    fix: correctly halt if sync ahead theshold is reached

--- executors/blocksync/db/db.go
+++ executors/blocksync/db/db.go
@@ -233,14 +233,16 @@ func StartDBExecutor(homePath, restEndpoint string, blockPoolId, targetHeight in
 		// if KSYNC has already fetched 2 * snapshot_interval ahead of the snapshot pool we wait
 		// in order to not bloat the KSYNC process
 		if snapshotInterval > 0 {
-			// if we are in that range we wait until the snapshot pool moved on
-			if block.Height > snapshotPoolHeight+(utils.SnapshotPruningAheadFactor*snapshotInterval) {
-				logger.Info().Msg("synced too far ahead of snapshot pool. Waiting for snapshot pool to catch up ...")
-				time.Sleep(10 * time.Second)
-				continue
-			}
+			for {
+				// if we are in that range we wait until the snapshot pool moved on
+				if block.Height > snapshotPoolHeight+(utils.SnapshotPruningAheadFactor*snapshotInterval) {
+					logger.Info().Msg("synced too far ahead of snapshot pool. Waiting for snapshot pool to catch up ...")
+					time.Sleep(10 * time.Second)
+					continue
+				}
 
-			break
+				break
+			}
 		}
 
 		if pruning && prevBlock.Height%utils.PruningInterval == 0 {
