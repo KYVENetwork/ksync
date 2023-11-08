95a13dafb8db4deba06997fa8f5a35e579b0fd7c
Troy Kessler <troy.kessler99@gmail.com>
2023-11-08 14:01:59 +0100

    chore: added engine interface descriptions

--- types/interfaces.go
+++ types/interfaces.go
@@ -1,5 +1,7 @@
 package types
 
+// Engine is an interface defining common behaviour for each consensus engine.
+// Currently, both tendermint and cometbft are supported
 type Engine interface {
 	// Start starts the engine and performs setups,
 	// should be called before every other method
@@ -28,34 +30,52 @@ type Engine interface {
 	// the app
 	ApplyBlock(value []byte) error
 
+	// ApplyFirstBlockOverP2P applies the first block over the P2P reactor
+	// which is necessary, if the genesis file is bigger than 100MB
 	ApplyFirstBlockOverP2P(value, nextValue []byte) error
 
+	// GetGenesisPath gets the file path to the genesis file
 	GetGenesisPath() string
 
+	// GetGenesisHeight gets the initial height defined by the genesis file
 	GetGenesisHeight() (int64, error)
 
+	// GetHeight gets the latest height stored in the blockstore.db
 	GetHeight() int64
 
+	// GetBaseHeight gets the earliest height stored in the blockstore.db
 	GetBaseHeight() int64
 
+	// GetAppHeight gets over ABCI the latest block height tracked by the app
 	GetAppHeight() (int64, error)
 
+	// GetSnapshots gets the available snapshots over ABCI from the app
 	GetSnapshots() ([]byte, error)
 
+	// IsSnapshotAvailable gets available snapshots over ABCI from the app
+	// and checks if the requested snapshot is available
 	IsSnapshotAvailable(height int64) (bool, error)
 
+	// GetSnapshotChunk gets the requested snapshot chunk over ABCI from the
+	// app
 	GetSnapshotChunk(height, format, chunk int64) ([]byte, error)
 
+	// GetBlock loads the requested block from the blockstore.db
 	GetBlock(height int64) ([]byte, error)
 
+	// GetState rebuilds the requested state from the blockstore and state.db
 	GetState(height int64) ([]byte, error)
 
+	// GetSeenCommit loads the seen commit from the blockstore.db
 	GetSeenCommit(height int64) ([]byte, error)
 
+	// OfferSnapshot offers a snapshot over ABCI to the app
 	OfferSnapshot(value []byte) (string, uint32, error)
 
+	// ApplySnapshotChunk applies a snapshot chunk over ABCI to the app
 	ApplySnapshotChunk(chunkIndex uint32, value []byte) (string, error)
 
+	// BootstrapState initializes the tendermint state
 	BootstrapState(value []byte) error
 
 	// PruneBlocks prunes blocks from the block store and state store
