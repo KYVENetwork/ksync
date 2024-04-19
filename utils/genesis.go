0451c1542aec6bbfeba6c633c4ad2d8ea1763bf1
Troy Kessler <troy.kessler99@gmail.com>
2024-04-19 08:52:11 +0200

    fix: only format genesis file if initial_height is an integer

--- utils/genesis.go
+++ utils/genesis.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func FormatGenesisFile(genesisPath string) error {
@@ -12,20 +13,24 @@ func FormatGenesisFile(genesisPath string) error {
 		return fmt.Errorf("error opening genesis.json at %s: %w", genesisPath, err)
 	}
 
-	var genesis map[string]interface{}
 	var value struct {
-		InitialHeight json.Number `json:"initial_height"`
+		InitialHeight int `json:"initial_height"`
 	}
+
+	// if we can not unmarshal the initial_height to an integer it means
+	// that it is of type string which is desired. In this case we return
+	// and don't format the genesis file
+	if err := json.Unmarshal(genesisFile, &value); err != nil {
+		return nil
+	}
+
+	var genesis map[string]interface{}
 
 	if err := json.Unmarshal(genesisFile, &genesis); err != nil {
 		return fmt.Errorf("failed to unmarshal genesis file: %w", err)
 	}
 
-	if err := json.Unmarshal(genesisFile, &value); err != nil {
-		return fmt.Errorf("failed to unmarshal initial_height value of genesis file: %w", err)
-	}
-
-	genesis["initial_height"] = value.InitialHeight.String()
+	genesis["initial_height"] = strconv.Itoa(value.InitialHeight)
 
 	genesisJson, err := json.MarshalIndent(genesis, "", "  ")
 	if err != nil {
