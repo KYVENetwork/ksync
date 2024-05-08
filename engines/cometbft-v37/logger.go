7bb76643a419c4fbcec59ffb72a9150f51b4e184
Troy Kessler <troy.kessler99@gmail.com>
2024-05-08 09:43:03 +0200

    fix: log app hash and hashes in hex format

--- engines/cometbft-v37/logger.go
+++ engines/cometbft-v37/logger.go
@@ -22,7 +22,11 @@ func (l KsyncCometLogger) Info(msg string, keyvals ...interface{}) {
 	logger := l.logger.Info()
 
 	for i := 0; i < len(keyvals); i = i + 2 {
-		logger = logger.Str(fmt.Sprintf("%v", keyvals[i]), fmt.Sprintf("%v", keyvals[i+1]))
+		if keyvals[i] == "hash" || keyvals[i] == "appHash" {
+			logger = logger.Str(fmt.Sprintf("%v", keyvals[i]), fmt.Sprintf("%x", keyvals[i+1]))
+		} else {
+			logger = logger.Str(fmt.Sprintf("%v", keyvals[i]), fmt.Sprintf("%v", keyvals[i+1]))
+		}
 	}
 
 	logger.Msg(msg)
