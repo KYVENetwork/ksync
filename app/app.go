7ff21bf82a57560f783e6292fb4a724f85ea3ac6
Troy Kessler <troy.kessler99@gmail.com>
2025-01-31 11:30:55 +0100

    chore: debug

--- app/app.go
+++ app/app.go
@@ -383,7 +383,7 @@ func (app *CosmosApp) LoadHomePath() error {
 
 	cmd := exec.Command(app.binaryPath)
 	fmt.Println(fmt.Sprintf("LD_LIBRARY_PATH=%s", app.getLDLibraryPath()))
-	cmd.Env = append(cmd.Env, fmt.Sprintf("LD_LIBRARY_PATH=%s", app.getLDLibraryPath()))
+	cmd.Env = append(os.Environ(), fmt.Sprintf("LD_LIBRARY_PATH=%s", app.getLDLibraryPath()))
 
 	if app.isCosmovisor {
 		cmd.Args = append(cmd.Args, "run")
