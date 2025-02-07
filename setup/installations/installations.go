bb00b5933d098216fd9af174fe83bbf752232ab8
Troy Kessler <troy.kessler99@gmail.com>
2025-02-07 08:57:32 +0100

    fix: libwasm path for beta versions

--- setup/installations/installations.go
+++ setup/installations/installations.go
@@ -243,6 +243,10 @@ func buildUpgradeBinary(upgrade types.Upgrade, chainSchema *types.ChainSchema, o
 		libwasmVersions := strings.Split(upgrade.LibwasmVersion, ".")
 		if libwasmVersions[0] == "v1" && libwasmVersions[1] == "0" {
 			libwasmPath = fmt.Sprintf("/go/pkg/mod/github.com/!cosm!wasm/wasmvm@%s/api/libwasmvm.x86_64.so", upgrade.LibwasmVersion)
+
+			if strings.Contains(upgrade.LibwasmVersion, "beta") {
+				libwasmPath = fmt.Sprintf("/go/pkg/mod/github.com/!cosm!wasm/wasmvm@%s/api/libwasmvm.so", upgrade.LibwasmVersion)
+			}
 		}
 	}
 
